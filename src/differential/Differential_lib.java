c5b6e3af0fcee635bdfc00480327ed4ade067a2c
sho <sho@192.168.179.2>
2017-11-20 12:43:16 +0900

    differential

---
+++ src/differential/Differential_lib.java
@@ -0,0 +1,33 @@
+package differential;
+
+public class Differential_lib implements Differential_if{
+
+	private double a, g, h;
+	
+	public Differential_lib(double a, double h) {
+		super();
+		this.a = a;
+		this.h = h;
+	}
+
+	@Override
+	public double getRx() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 2*this.a;
+	}
+
+	@Override
+	public double getDx() {
+		// TODO 自動生成されたメソッド・スタブ
+		return (Math.pow(this.a+this.h, 2)-Math.pow(this.a, 2))/h;
+	}
+	
+	double gosa() {
+		if(getRx() > getDx()) {
+			g = (getRx() - getDx()) / getRx() * 100;
+		}else {
+			g = (getDx() - getRx()) / getRx() * 100;
+		}
+		return g;
+	}
+}
---
+++ src/differential/Differential_main.java
@@ -0,0 +1,20 @@
+package differential;
+
+import java.util.Scanner;
+
+public class Differential_main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input value of a> ");
+		double a = Double.parseDouble(scan.next());
+		System.out.print("Input value of h> ");
+		double h = Double.parseDouble(scan.next());
+
+		Differential_lib dlib = new Differential_lib(a,h);
+		System.out.println("f(x)=x^2のx="+a+"における微分係数は"+dlib.getRx());
+		System.out.println("f(x)=x^2のx="+a+"における微分係数は"+dlib.getDx());
+		System.out.println("f(x)=x^2のx="+a+"における微分係数の相対誤差は"+dlib.gosa());
+	}
+}
